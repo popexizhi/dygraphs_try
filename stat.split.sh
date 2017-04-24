55d8d1cc64e9e77b29229e1551ed892a51b2fbec
popexizhi@216 <popexizhi@gmail.com>
2017-04-24 12:57:53 +0800

    更新split_iostat过滤方式，将时间将符修改为time+1

--- stat.split.sh
+++ stat.split.sh
@@ -4,10 +4,10 @@
 split_iostat(){
     fp=$1
     time=`cat ${fp}|head -n 1`
-    filelist=`cat ${fp}|grep -v "Device:"|grep -v CPU|sed 's/ \+/,/g'`
+    filelist=`cat ${fp}|sed 's/^$/time+1/g'|grep -v "Device:"|grep -v CPU|sed 's/ \+/,/g'`
     for i in ${filelist}
     do
-        echo "${time},${i}"
+        echo "${i}"
     done
 }
 
