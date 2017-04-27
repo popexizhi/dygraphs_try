ed80427fb6dcf38f2890e9dbeb91fe82023b805f
popexizhi@216 <popexizhi@gmail.com>
2017-04-27 18:42:15 +0800

    add all report

--- savehtml.sh
+++ savehtml.sh
@@ -3,9 +3,12 @@ get_html(){
     ip=$1
     savedir=$2
     ngdir="/data2/spotlight_web"
-    fp=`python html/activity_mod.py ${ip} ${savedir}`
-    mv ${fp} ${ngdir}
-    echo "http://192.168.1.216/spotlight_web/${fp}"
+    fplist=`python html/activity_mod.py ${ip} ${savedir}`
+    mv ${fplist} ${ngdir}
+    for fp in ${fplist}
+    do
+        echo "http://192.168.1.216/spotlight_web/${fp}"
+    done
 }
 L2_host(){
     iplist="192.168.1.80 192.168.1.207 192.168.1.73 192.168.1.74 192.168.1.113 192.168.1.114 192.168.1.212 172.16.101.3 192.168.1.56 192.168.1.58"
