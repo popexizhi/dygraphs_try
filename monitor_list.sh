63e7137eadb52ac6ef725bfc72e48dc587cab663
popexizhi@216 <popexizhi@gmail.com>
2017-05-04 17:37:57 +0800

    add Relay_test test is pass

--- monitor_list.sh
+++ monitor_list.sh
@@ -31,3 +31,20 @@ Load_test_use(){
     ssh_all 1 slim password 192.168.1.56 &
     ssh_all 1 slim password 192.168.1.58 &
 }
+Relay_test_use(){
+    ssh_all 1 slim abc123,./ 192.168.1.80 &
+    ssh_all 1 slim abc123,./ 192.168.1.84 &
+    ssh_all 1 slim abc123,./ 192.168.1.207 &
+    ssh_all 1 slim password 192.168.1.113 &
+    ssh_all 1 slim abc123,./ 192.168.1.73 &
+    ssh_all 1 slim abc123,./ 192.168.1.74 &
+    ssh_all 1 slim password 192.168.1.114 &
+    ssh_all 1 slim abc123,./ 192.168.1.75 &
+    ssh_all 1 slim abc123,./ 192.168.1.76 &
+    ssh_all 1 slim password 192.168.1.99 &
+    ssh_all 1 slim abc123,./ 192.168.1.212 &
+    ssh_all 1 slim abc123,./ 192.168.1.208 &
+    ssh_all 1 slim password 172.16.101.3 &
+    ssh_all 1 slim password 192.168.1.56 &
+    ssh_all 1 slim password 192.168.1.58 &
+}
